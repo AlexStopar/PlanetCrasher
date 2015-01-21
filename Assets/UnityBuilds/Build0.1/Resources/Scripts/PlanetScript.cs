590cf04b416c9120c2df6dd4ced9d3bdcfe97db3
Alex Stopar <stopar.2@osu.edu>
2015-01-21 18:12:21 -0500

    Created ObstaclePlanet Movement
    Made variables for speed and external planet boundaries

--- Assets/UnityBuilds/Build0.1/Resources/Scripts/PlanetScript.cs
+++ Assets/UnityBuilds/Build0.1/Resources/Scripts/PlanetScript.cs
@@ -3,7 +3,9 @@ using System.Collections;
 
 public class PlanetScript : MonoBehaviour {
 
-
+	public bool isMoving = false;
+	public float EXTERNAL_SIGHT_POINT = 6.0f;
+	public float PLANETARY_SPEED = 0.1f;
 	// Use this for initialization
 	void Start () {
 
@@ -16,6 +18,12 @@ public class PlanetScript : MonoBehaviour {
 	}
 	// Update is called once per frame
 	void Update () {
-
+		if(isMoving)
+		{
+			gameObject.transform.Translate(Vector3.left * PLANETARY_SPEED);
+			if (gameObject.transform.position.x < -EXTERNAL_SIGHT_POINT) 
+				gameObject.transform.position = new Vector3(EXTERNAL_SIGHT_POINT, 
+				                                            gameObject.transform.position.y, gameObject.transform.position.z);
+		}
 	}
 }

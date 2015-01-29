f46afa825f85a95f63995babb7f6d88548634ddc
Alex Stopar <stopar.2@osu.edu>
2015-01-28 23:49:01 -0500

    Made oval shape of moon scaling
    Set range for full size and smallest size to a larger size

--- Assets/UnityBuilds/Build0.1/Resources/Scripts/MoonScript.cs
+++ Assets/UnityBuilds/Build0.1/Resources/Scripts/MoonScript.cs
@@ -6,6 +6,7 @@ public class MoonScript : MonoBehaviour {
 
 	public float ORBIT_SPEED = 0.05f;
 	public float ORBIT_RADIUS = 2.0f;
+	public float CLAMP_RADIUS = 1.0f;
 	public float MOON_SCALER = 0.98f;
 	public float CURVE_TIP_OFFSET = 0.5f;
 	float initX = 0.0f;
@@ -15,8 +16,10 @@ public class MoonScript : MonoBehaviour {
 	float curveTip = 0.0f;
 	float orbitProgress = 0.0f;
 	bool isBehindPlanet = false;
+	bool isShrinking = false;
+	bool isGrowing = false;
 	bool isLeftOfCenter = true;
-	Vector3 initialScale;
+	Vector3 currentScale;
 	void OnTriggerEnter2D (Collider2D asteroid) 
 	{
 		if(asteroid.gameObject == null || !asteroid.gameObject.name.Contains("Asteroid")) return;
@@ -25,14 +28,13 @@ public class MoonScript : MonoBehaviour {
 
 	void Awake()
 	{
-		initialScale = gameObject.transform.localScale;
+		currentScale = gameObject.transform.localScale;
 		initX = gameObject.transform.position.x;
 		curveTip = gameObject.transform.position.y - CURVE_TIP_OFFSET;
 		centerX = gameObject.transform.parent.position.x;
 		if(initX > centerX) isLeftOfCenter = false;
 		else isLeftOfCenter = true;
 		orbitProgress = Mathf.Abs (centerX - initX);
-		gameObject.transform.localScale = Mathf.Pow (MOON_SCALER, orbitProgress / ORBIT_SPEED) * initialScale;
 	}
 
 	void Start()
@@ -53,13 +55,27 @@ public class MoonScript : MonoBehaviour {
 		if(isLeftOfCenter) orbitProgress += ORBIT_SPEED;
 		else orbitProgress -= ORBIT_SPEED;
 		initX = gameObject.transform.position.x;
-		gameObject.transform.localScale = Mathf.Pow (MOON_SCALER, orbitProgress / ORBIT_SPEED) * initialScale;
 
 		if(initX < centerX) isLeftOfCenter = true;
 		else if(initX > centerX) isLeftOfCenter = false;
 
 		if(orbitProgress >= ORBIT_RADIUS) isBehindPlanet = true;
 		else isBehindPlanet = false;
+
+		if(orbitProgress > CLAMP_RADIUS && orbitProgress < (2.0f * CLAMP_RADIUS))
+		{
+			if(isLeftOfCenter) isShrinking = true;
+			else isGrowing = true;
+		}
+		else 
+		{
+			isShrinking = false;
+			isGrowing = false;
+		}
+
+		if (isShrinking) currentScale = currentScale * MOON_SCALER;
+		else if (isGrowing) currentScale = currentScale / MOON_SCALER;
+		gameObject.transform.localScale = currentScale;
 
 		if (isBehindPlanet) { //Set zPosition and yPosition of moon based on parabolic curve and position behind or in front of planet
 			zPosition = gameObject.transform.parent.position.z + 1.0f;

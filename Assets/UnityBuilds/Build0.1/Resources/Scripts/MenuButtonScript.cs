2b46f4aa3e7d578868d353b280aecd14940179ec
Alex Stopar <stopar.2@osu.edu>
2015-02-20 11:00:06 -0500

    Added menu with level loading
    Next comes HP dwindling of asteroids.

---
+++ Assets/UnityBuilds/Build0.1/Resources/Scripts/MenuButtonScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class MenuButtonScript : MonoBehaviour {
+
+	BoxCollider2D StartButtonBounds;
+	BoxCollider2D TwoPlayerBounds;
+	Camera camera;
+	// Use this for initialization
+	void Start () {
+		StartButtonBounds = GameObject.Find ("StartButton").GetComponent<BoxCollider2D> ();
+		TwoPlayerBounds = GameObject.Find ("2PlayerButton").GetComponent<BoxCollider2D> ();
+		camera = GameObject.Find ("Main Camera").camera;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.touchCount >= 1) 
+		{
+				Vector3 touchPoint = camera.ScreenToWorldPoint (Input.GetTouch (0).position);
+				if(StartButtonBounds == Physics2D.OverlapPoint(touchPoint)) Application.LoadLevel ("testLevel");
+				else if(TwoPlayerBounds == Physics2D.OverlapPoint(touchPoint)) Application.LoadLevel("2PLevel");	   
+		}
+	}
+}
